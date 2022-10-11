6a1c84393645243857e0962c15ccfb5e2e6eb339
Fabrício Vaz <fabricio.vaz2004@gmail.com>
2022-10-11 15:27:11 -0300

    Segundo commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int num = 10;
-         for (int i = 1; i<=num; i++) {
+        System.out.print("Quantas linhas voce quer ver? ");
+        int linha = sc.nextInt();
+
+         for (int i = 1; i<=linha; i++) {
              for (int j = 1; j <= i; j++) {
                  System.out.print(i);
                  System.out.print(" ");
